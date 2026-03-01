bb6466e88196bd5a80caf93fc404d15838d6aeaf
Daniela Rojas Diaz <drojasd@unicesar.edu.co>
2026-02-28 23:46:21 -0500

    feat: agregar encapsulamiento a clase Estudiante

--- SistemaAcademicoLite/src/sistemaacademicolite/Estudiante.java
+++ SistemaAcademicoLite/src/sistemaacademicolite/Estudiante.java
@@ -6,4 +6,56 @@ public class Estudiante {
     String apellido;
     int edad;
     int semestre;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(String codigo, String nombre, String apellido, int edad, int semestre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.semestre = semestre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+    
 }
